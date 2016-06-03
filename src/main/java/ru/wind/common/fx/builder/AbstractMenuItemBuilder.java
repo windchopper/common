89a6b6a920261d1cf850ed182d08596edba50ce4
wind <wind@comtv.ru>
2016-06-04 02:36:36 +0300

    no commit message

--- src/main/java/ru/wind/common/fx/builder/AbstractMenuItemBuilder.java
+++ src/main/java/ru/wind/common/fx/builder/AbstractMenuItemBuilder.java
@@ -1,5 +1,6 @@
 package ru.wind.common.fx.builder;
 
+import javafx.beans.value.ObservableValue;
 import javafx.scene.Node;
 import javafx.scene.control.MenuItem;
 
@@ -19,6 +20,11 @@ public abstract class AbstractMenuItemBuilder<T extends MenuItem, B extends Abst
     @SuppressWarnings("unchecked") public B graphic(Node graphic) {
         target.setGraphic(graphic);
         return (B) this;
+    }
+
+    public B bindDisableProperty(ObservableValue<? extends Boolean> observable) {
+        target.disableProperty().bind(observable);
+        return self();
     }
 
 }
