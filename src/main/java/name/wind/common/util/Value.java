bc82d80fb9f6d9a3227f0d6f00f81404301d2bb9
wind <wind@comtv.ru>
2016-06-19 01:05:43 +0300

    no commit message

---
+++ src/main/java/name/wind/common/util/Value.java
@@ -0,0 +1,27 @@
+package name.wind.common.util;
+
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+
+public class Value<T> {
+
+    private final T value;
+
+    public Value(T value) {
+        this.value = value;
+    }
+
+    public static <T> Value<T> of(T value) {
+        return new Value<>(value);
+    }
+
+    public static <T> Value<T> of(Supplier<T> valueSupplier) {
+        return new Value<>(valueSupplier.get());
+    }
+
+    public Value<T> with(Consumer<? super T> consumer) {
+        consumer.accept(value);
+        return this;
+    }
+
+}
