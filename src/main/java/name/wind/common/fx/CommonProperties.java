6cec948989a36ba0876323ba241d26c219a467df
wind <wind@comtv.ru>
2016-06-19 21:16:27 +0300

    no commit message

--- src/main/java/name/wind/common/fx/CommonProperties.java
+++ src/main/java/name/wind/common/fx/CommonProperties.java
@@ -3,5 +3,6 @@ package name.wind.common.fx;
 public interface CommonProperties {
 
     String PROPERTY__IDENTIFIER = "common.fx.identifier";
+    String PROPERTY__PREFERRED_SIZE = "common.fx.preferred.size";
 
 }
--- src/main/java/name/wind/common/fx/behavior/WindowApplyStoredBoundsBehavior.java
+++ src/main/java/name/wind/common/fx/behavior/WindowApplyStoredBoundsBehavior.java
@@ -1,13 +1,13 @@
 package name.wind.common.fx.behavior;
 
 import javafx.application.Platform;
+import javafx.event.EventHandler;
+import javafx.geometry.Dimension2D;
 import javafx.geometry.Rectangle2D;
 import javafx.stage.Window;
+import javafx.stage.WindowEvent;
 import name.wind.common.fx.CommonProperties;
 import name.wind.common.fx.preferences.RectanglePreferencesEntry;
-import name.wind.common.preferences.PreferencesEntry;
-import name.wind.common.util.Optional;
-import name.wind.common.util.Value;
 
 import java.util.ResourceBundle;
 import java.util.logging.Level;
@@ -16,53 +16,73 @@ import java.util.logging.Logger;
 public class WindowApplyStoredBoundsBehavior implements Behavior<Window> {
 
     private static final ResourceBundle bundle = ResourceBundle.getBundle("common.i18n.messages");
-    private static final Logger logger = Logger.getLogger(
-        WindowApplyStoredBoundsBehavior.class.getName());
+    private static final Logger logger = Logger.getLogger(WindowApplyStoredBoundsBehavior.class.getName());
 
     @Override public void apply(Window window) {
-        window.sceneProperty().addListener(
-            (sceneProperty, oldScene, newScene) -> Platform.runLater(
-                () -> Optional.of(window.getProperties().get(CommonProperties.PROPERTY__IDENTIFIER))
-                    .ifNotPresent(() -> logger.log(Level.WARNING, bundle.getString("common.fx.behavior.WindowPreferencedBounds.missingIdentifier")))
-                    .map(identifier -> new RectanglePreferencesEntry(WindowApplyStoredBoundsBehavior.class, identifier + "Bounds"))
-                    .ifPresent(preferencesEntry -> Value.of(window)
-                        .with(target -> target.xProperty().addListener(
-                            (xProperty, oldX, newX) -> preferencesEntry.accept(
-                                new Rectangle2D(
-                                    newX.doubleValue(),
-                                    window.getY(),
-                                    window.getWidth(),
-                                    window.getHeight()))))
-                        .with(target -> target.yProperty().addListener(
-                            (yProperty, oldY, newY) -> preferencesEntry.accept(
-                                new Rectangle2D(
-                                    window.getX(),
-                                    newY.doubleValue(),
-                                    window.getWidth(),
-                                    window.getHeight()))))
-                        .with(target -> target.widthProperty().addListener(
-                            (widthProperty, oldWidth, newWidth) -> preferencesEntry.accept(
-                                new Rectangle2D(
-                                    window.getX(),
-                                    window.getY(),
-                                    newWidth.doubleValue(),
-                                    window.getHeight()))))
-                        .with(target -> target.heightProperty().addListener(
-                            (heightProperty, oldHeight, newHeight) -> preferencesEntry.accept(
-                                new Rectangle2D(
-                                    window.getX(),
-                                    window.getY(),
-                                    window.getWidth(),
-                                    newHeight.doubleValue())))))
-                    .map(PreferencesEntry::get)
-                    .ifNotPresent(() -> Value.of(window)
-                        .with(Window::sizeToScene)
-                        .with(Window::centerOnScreen))
-                    .ifPresent(bounds -> Value.of(window)
-                        .with(target -> target.setX(bounds.getMinX()))
-                        .with(target -> target.setY(bounds.getMinY()))
-                        .with(target -> target.setWidth(bounds.getWidth()))
-                        .with(target -> target.setHeight(bounds.getHeight())))));
+        window.sceneProperty().addListener((sceneProperty, oldScene, newScene) -> Platform.runLater(() -> {
+            String identifier = (String) window.getProperties().get(CommonProperties.PROPERTY__IDENTIFIER);
+            Dimension2D preferredSize = (Dimension2D) window.getProperties().get(CommonProperties.PROPERTY__PREFERRED_SIZE);
+            Rectangle2D bounds = null;
+
+            if (identifier == null) {
+                logger.log(Level.WARNING, bundle.getString("common.fx.behavior.WindowPreferencedBounds.missingIdentifier"));
+            } else {
+                RectanglePreferencesEntry preferencesEntry = new RectanglePreferencesEntry(WindowApplyStoredBoundsBehavior.class, identifier + "Bounds");
+                bounds = preferencesEntry.get();
+
+                window.addEventHandler(WindowEvent.WINDOW_SHOWN, new EventHandler<WindowEvent>() {
+                    @Override public void handle(WindowEvent event) {
+                        window.removeEventHandler(WindowEvent.WINDOW_SHOWN, this);
+
+                        window.xProperty().addListener((property, oldX, newX) -> preferencesEntry.accept(
+                            new Rectangle2D(
+                                newX.doubleValue(),
+                                window.getY(),
+                                window.getWidth(),
+                                window.getHeight())));
+
+                        window.yProperty().addListener((property, oldY, newY) -> preferencesEntry.accept(
+                            new Rectangle2D(
+                                window.getX(),
+                                newY.doubleValue(),
+                                window.getWidth(),
+                                window.getHeight())));
+
+                        window.widthProperty().addListener((property, oldWidth, newWidth) -> preferencesEntry.accept(
+                            new Rectangle2D(
+                                window.getX(),
+                                window.getY(),
+                                newWidth.doubleValue(),
+                                window.getHeight())));
+
+                        window.heightProperty().addListener((property, oldHeight, newHeight) -> preferencesEntry.accept(
+                            new Rectangle2D(
+                                window.getX(),
+                                window.getY(),
+                                window.getWidth(),
+                                newHeight.doubleValue())));
+                    }
+                });
+            }
+
+            if (bounds != null) {
+                window.setX(bounds.getMinX());
+                window.setY(bounds.getMinY());
+                window.setWidth(bounds.getWidth());
+                window.setHeight(bounds.getHeight());
+            } else if (preferredSize != null) {
+                window.setWidth(preferredSize.getWidth());
+                window.setHeight(preferredSize.getHeight());
+            }
+
+            if (bounds == null && preferredSize == null) {
+                window.sizeToScene();
+            }
+
+            if (bounds == null) {
+                window.centerOnScreen();
+            }
+        }));
     }
 
 }
