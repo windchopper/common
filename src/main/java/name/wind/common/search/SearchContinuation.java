1280da91e392ac3bfb938e390ef6cd71a465d734
wind <wind@comtv.ru>
2016-07-29 01:45:32 +0300

    no commit message

--- src/main/java/name/wind/common/search/SearchContinuation.java
+++ src/main/java/name/wind/common/search/SearchContinuation.java
@@ -7,4 +7,27 @@ public interface SearchContinuation<T, C> {
 
     void found(C context, T found) throws SearchStoppedException;
 
+    class SimpleContinuation implements SearchContinuation<Object, Object> {
+
+        private Object searchResult;
+
+        @Override public Object newContext() {
+            return null;
+        }
+
+        @Override public Object deriveContext(Object context, Object tail) {
+            return null;
+        }
+
+        @Override public void found(Object context, Object found) throws SearchStoppedException {
+            searchResult = found;
+            throw new SearchStoppedException();
+        }
+
+        public Object searchResult() {
+            return searchResult;
+        }
+
+    }
+
 }
