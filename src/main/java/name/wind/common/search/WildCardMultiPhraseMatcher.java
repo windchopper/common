875891b8ea06678f8508b80faecedca73284460e
wind <wind@comtv.ru>
2016-08-02 19:41:31 +0300

    no commit message

---
+++ src/main/java/name/wind/common/search/WildCardMultiPhraseMatcher.java
@@ -0,0 +1,62 @@
+package name.wind.common.search;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class WildCardMultiPhraseMatcher {
+
+    private final Pattern quotedPhrasePattern = Pattern.compile("(?<all>[\"](?<text>.+?)[\"])");
+    private final Pattern simplePhrasePattern = Pattern.compile("(?<text>[^\\p{Z}\"]+)");
+
+    private final Pattern combinedPattern;
+
+    public WildCardMultiPhraseMatcher(String searchPhrase) {
+        Map<String, String> quotedPhrases = new HashMap<>();
+        Set<String> phrases = new HashSet<>();
+        Set<String> expressions = new HashSet<>();
+
+        while (true) {
+            Matcher matcher = quotedPhrasePattern.matcher(searchPhrase);
+
+            if (matcher.find()) {
+                String replacement = Long.toHexString(System.nanoTime());
+                quotedPhrases.put(replacement, "\\Q" + matcher.group("text") + "\\E");
+                searchPhrase = matcher.replaceFirst(replacement);
+            } else {
+                break;
+            }
+        }
+
+        Matcher matcher = simplePhrasePattern.matcher(searchPhrase);
+
+        for (int index = 0; matcher.find(index); index = matcher.end()) {
+            phrases.add(matcher.group("text"));
+        }
+
+        for (String phrase : phrases) {
+            for (Map.Entry<String, String> quotedPhraseEntry : quotedPhrases.entrySet()) {
+                phrase = phrase.replace(
+                    quotedPhraseEntry.getKey(),
+                    quotedPhraseEntry.getValue());
+            }
+
+            expressions.add(
+                "(" + phrase.replace("*", ".*").replace("?", ".") + ")");
+        }
+
+        combinedPattern = Pattern.compile(
+            String.join("|", expressions), Pattern.CASE_INSENSITIVE);
+    }
+
+    public <T> Predicate<T> toPredicate(Function<T, String> stringifier) {
+        return object -> combinedPattern.matcher(
+            stringifier.apply(object)).find();
+    }
+
+}
---
+++ src/test/java/name/wind/common/search/WildCardMultiPhraseMattcherTest.java
@@ -0,0 +1,22 @@
+package name.wind.common.search;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.function.Predicate;
+
+public class WildCardMultiPhraseMattcherTest {
+
+    @Test public void testSearch() {
+        String searchPhrase = "abc1 de* \"ghi\"* j??4 \"3 j\"";
+        WildCardMultiPhraseMatcher matcher = new WildCardMultiPhraseMatcher(searchPhrase);
+        Predicate<Object> predicate = matcher.toPredicate(Object::toString);
+
+        Assert.assertTrue(predicate.test("abc1"));
+        Assert.assertTrue(predicate.test("def2"));
+        Assert.assertTrue(predicate.test("ghi3"));
+        Assert.assertTrue(predicate.test("jkl4"));
+        Assert.assertTrue(predicate.test("abc1 def2 ghi3 jkl4"));
+    }
+
+}
