7095ebc11eedecbe005a2ec7758a9f819ff33dd4
wind <wind@comtv.ru>
2016-08-06 19:48:38 +0300

    no commit message

--- src/main/java/name/wind/common/util/KnownSystemProperties.java
+++ src/main/java/name/wind/common/util/KnownSystemProperties.java
@@ -1,13 +1,17 @@
 package name.wind.common.util;
 
+import java.io.File;
+
 public interface KnownSystemProperties {
 
     String NAME__OS_ARCHITECTURE = "os.arch";
     String NAME__OS_VERSION = "os.version";
     String NAME__OS_NAME = "os.name";
+    String NAME__USER_HOME = "user.home";
 
     SystemProperty<String> PROPERTY__OS_ARCHITECTURE = new SystemProperty<>(NAME__OS_ARCHITECTURE, stringValue -> stringValue);
     SystemProperty<String> PROPERTY__OS_VERSION = new SystemProperty<>(NAME__OS_VERSION, stringValue -> stringValue);
     SystemProperty<String> PROPERTY__OS_NAME = new SystemProperty<>(NAME__OS_NAME, stringValue -> stringValue);
+    SystemProperty<File> PROPERTY__USER_HOME = new SystemProperty<>(NAME__USER_HOME, File::new);
 
 }
