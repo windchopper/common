75a418989779362e71be17476725ef0d2b3144ea
wind <wind@comtv.ru>
2017-03-05 03:24:53 +0300

    no commit message

---
+++ src/main/java/com/github/windchopper/common/annotations/BundleStringLiteral.java
@@ -0,0 +1,48 @@
+package com.github.windchopper.common.annotations;
+
+import java.lang.annotation.Annotation;
+
+public class BundleStringLiteral implements BundleString {
+
+    private String bundleKey;
+    private String bundleLocation;
+
+    public BundleStringLiteral(String bundleKey, String bundleLocation) {
+        this.bundleKey = bundleKey;
+        this.bundleLocation = bundleLocation;
+    }
+
+    public BundleStringLiteral(BundleString bundleString) {
+        this(
+            bundleString.bundleKey(),
+            bundleString.bundleLocation());
+    }
+
+    @Override
+    public Class<? extends Annotation> annotationType() {
+        return BundleString.class;
+    }
+
+    @Override
+    public String bundleKey() {
+        return bundleKey;
+    }
+
+    public String bundleKey(String newBundleKey) {
+        String oldBundleKey = bundleKey;
+        bundleKey = newBundleKey;
+        return oldBundleKey;
+    }
+
+    @Override
+    public String bundleLocation() {
+        return bundleLocation;
+    }
+
+    public String bundleLocation(String newBundleLocation) {
+        String oldBundleLocation = bundleLocation;
+        bundleLocation = newBundleLocation;
+        return oldBundleLocation;
+    }
+
+}
