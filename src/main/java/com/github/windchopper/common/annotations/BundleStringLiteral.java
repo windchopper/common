9142e78393b098ce9cf7d385cc8f66ac88544b61
wind <wind@comtv.ru>
2017-03-05 03:59:18 +0300

    no commit message

--- src/main/java/com/github/windchopper/common/annotations/BundleStringLiteral.java
+++ src/main/java/com/github/windchopper/common/annotations/BundleStringLiteral.java
@@ -1,6 +1,8 @@
 package com.github.windchopper.common.annotations;
 
+import javax.annotation.Nonnull;
 import java.lang.annotation.Annotation;
+import java.util.Objects;
 
 @SuppressWarnings("ClassExplicitlyAnnotation")
 public class BundleStringLiteral implements BundleString {
@@ -44,6 +46,22 @@ public class BundleStringLiteral implements BundleString {
         String oldBundleLocation = bundleLocation;
         bundleLocation = newBundleLocation;
         return oldBundleLocation;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(bundleKey, bundleLocation);
+    }
+
+    boolean equals(@Nonnull BundleStringLiteral that) {
+        return Objects.equals(that.bundleKey, bundleKey)
+            && Objects.equals(that.bundleLocation, bundleLocation);
+    }
+
+    @Override
+    public boolean equals(Object that) {
+        return that != null && that.getClass() == getClass() && (
+            that == this || equals((BundleStringLiteral) that));
     }
 
 }
