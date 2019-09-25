55c68e8cd650b853cfbbd81df134e6400ae633c4
wind <wind@comtv.ru>
2019-09-26 01:41:35 +0300

    #

--- src/main/java/com/github/windchopper/common/cdi/SystemPropertyProducer.java
+++ src/main/java/com/github/windchopper/common/cdi/SystemPropertyProducer.java
@@ -1,17 +1,26 @@
 package com.github.windchopper.common.cdi;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.context.Dependent;
 import javax.enterprise.inject.Produces;
 import javax.enterprise.inject.spi.InjectionPoint;
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @ApplicationScoped
 public class SystemPropertyProducer extends Producer {
 
-    @Produces
-    @ApplicationScoped
-    @SystemProperty("any")
-    public String produceStringSystemProperty(InjectionPoint injectionPoint) {
+    @Produces @Dependent @SystemProperty("any") public String produceStringSystemProperty(InjectionPoint injectionPoint) {
         return System.getProperty(findQualifier(injectionPoint, SystemProperty.class).value());
+    }
+
+    @Produces @Dependent @SystemProperty("any") public Path producePathSystemProperty(InjectionPoint injectionPoint) {
+        return Paths.get(System.getProperty(findQualifier(injectionPoint, SystemProperty.class).value()));
+    }
+
+    @Produces @Dependent @SystemProperty("any") public File produceFileSystemProperty(InjectionPoint injectionPoint) {
+        return new File(System.getProperty(findQualifier(injectionPoint, SystemProperty.class).value()));
     }
 
 }
