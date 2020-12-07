7af808d71cfc2022815edb5b2936562d0e9121b5
wind <wind@comtv.ru>
2020-12-08 01:05:17 +0300

    #

--- common-fx/src/main/java/com/github/windchopper/common/fx/preferences/PointPreferencesEntryType.java
+++ common-fx/src/main/java/com/github/windchopper/common/fx/preferences/PointPreferencesEntryType.java
@@ -1,12 +1,13 @@
 package com.github.windchopper.common.fx.preferences;
 
+import com.github.windchopper.common.preferences.types.StructuralType;
 import com.github.windchopper.common.util.Pipeliner;
 import javafx.geometry.Point2D;
 
 import javax.json.Json;
 import javax.json.JsonObjectBuilder;
 
-public class PointPreferencesEntryType extends DoubleAwareStructuralType<Point2D> {
+public class PointPreferencesEntryType extends StructuralType<Point2D> {
 
     private static final String KEY__X = "x";
     private static final String KEY__Y = "y";
@@ -14,11 +15,11 @@ public class PointPreferencesEntryType extends DoubleAwareStructuralType<Point2D
     public PointPreferencesEntryType() {
         super(
             source -> new Point2D(
-                readDouble(source, KEY__X),
-                readDouble(source, KEY__Y)),
+                source.getJsonNumber(KEY__X).doubleValue(),
+                source.getJsonNumber(KEY__Y).doubleValue()),
             source -> Pipeliner.of(Json::createObjectBuilder)
-                .set(target -> value -> target.add(KEY__X, value), String.valueOf(source.getX()))
-                .set(target -> value -> target.add(KEY__Y, value), String.valueOf(source.getY()))
+                .set(target -> value -> target.add(KEY__X, value), source.getX())
+                .set(target -> value -> target.add(KEY__Y, value), source.getY())
                 .map(JsonObjectBuilder::build)
                 .get());
     }
--- common-fx/src/main/java/com/github/windchopper/common/fx/preferences/RectanglePreferencesEntryType.java
+++ common-fx/src/main/java/com/github/windchopper/common/fx/preferences/RectanglePreferencesEntryType.java
@@ -1,12 +1,13 @@
 package com.github.windchopper.common.fx.preferences;
 
+import com.github.windchopper.common.preferences.types.StructuralType;
 import com.github.windchopper.common.util.Pipeliner;
 import javafx.geometry.Rectangle2D;
 
 import javax.json.Json;
 import javax.json.JsonObjectBuilder;
 
-public class RectanglePreferencesEntryType extends DoubleAwareStructuralType<Rectangle2D> {
+public class RectanglePreferencesEntryType extends StructuralType<Rectangle2D> {
 
     private static final String KEY__X = "x";
     private static final String KEY__Y = "y";
@@ -16,15 +17,15 @@ public class RectanglePreferencesEntryType extends DoubleAwareStructuralType<Rec
     public RectanglePreferencesEntryType() {
         super(
             source -> new Rectangle2D(
-                readDouble(source, KEY__X),
-                readDouble(source, KEY__Y),
-                readDouble(source, KEY__WIDTH),
-                readDouble(source, KEY__HEIGHT)),
+                source.getJsonNumber(KEY__X).doubleValue(),
+                source.getJsonNumber(KEY__Y).doubleValue(),
+                source.getJsonNumber(KEY__WIDTH).doubleValue(),
+                source.getJsonNumber(KEY__HEIGHT).doubleValue()),
             source -> Pipeliner.of(Json::createObjectBuilder)
-                .set(target -> value -> target.add(KEY__X, value), String.valueOf(source.getMinX()))
-                .set(target -> value -> target.add(KEY__Y, value), String.valueOf(source.getMinY()))
-                .set(target -> value -> target.add(KEY__WIDTH, value), String.valueOf(source.getWidth()))
-                .set(target -> value -> target.add(KEY__HEIGHT, value), String.valueOf(source.getHeight()))
+                .set(target -> value -> target.add(KEY__X, value), source.getMinX())
+                .set(target -> value -> target.add(KEY__Y, value), source.getMinY())
+                .set(target -> value -> target.add(KEY__WIDTH, value), source.getWidth())
+                .set(target -> value -> target.add(KEY__HEIGHT, value), source.getHeight())
                 .map(JsonObjectBuilder::build)
                 .get());
     }
